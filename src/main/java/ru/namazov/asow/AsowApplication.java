f7986cb2d3c0e4daf46f532d80826f467615e7c8
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-05-10 21:32:55 +0300

    initcial commit

---
+++ src/main/java/ru/namazov/asow/AsowApplication.java
@@ -0,0 +1,13 @@
+package ru.namazov.asow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/namazov/asow/AsowApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.namazov.asow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
