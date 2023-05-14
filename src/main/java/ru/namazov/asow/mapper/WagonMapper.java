3f52835129a4a8a29c305eab810efda2da23b621
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-05-14 15:08:17 +0300

    Доделал контрольлеры

---
+++ src/main/java/ru/namazov/asow/mapper/WagonMapper.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023, TopS BI LLC. All rights reserved.
+ * http://www.topsbi.ru
+ */
+
+package ru.namazov.asow.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import ru.namazov.asow.dto.WagonDTO;
+import ru.namazov.asow.entity.Wagon;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface WagonMapper {
+
+    Wagon toEntity(WagonDTO wagonDTO);
+
+    WagonDTO toDTO(Wagon wagon);
+
+    List<Wagon> toEntity(List<WagonDTO> wagonDTOList);
+}
